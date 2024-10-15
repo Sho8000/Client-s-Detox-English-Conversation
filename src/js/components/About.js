a5e307e24b3d0cc86e6ec6b5fa62e562f00b58c6
Sho8000 <yoshikichi8000@gmail.com>
2024-10-15 13:32:01 -0700

    moved long message into Variable.js

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -1,18 +1,5 @@
 import { Component } from "../common/component.js";
-
-const textTitle = ["Akiちゃん","CAライフ"];
-const textContent = [`こんにちは、あきちゃんです
-              私は九州の城下町、熊本で魚屋の両親の元に次男（？）、長女（？）として生まれました。
-              ま、幼少期から男の子が好きだった訳なんですね。
-              小さい時から、植物と飛行機と英語が好きで、好きが嵩じて、園芸店で働いたり、CAになったり、英会話コーチをやって来ました。
-              プライベートでは妊活中でして、女性カップルに精子提供をすることで新しい形の家族を作ろうと奮闘しています。
-              そんなカラフルでモザイクな感じの私がみなさんに提供しているのが
-              「呪いから解放」するセッションです。
-              興味がある人はまず連絡してみてくださいね！`,`中でも最もexcitingだったのはCAライフ（実際はキャビンクルーと呼びます）ですね。タイの航空会社とドバイの航空会社に居ましたが、日々、世界１４０都市をまたにかける生活でした。
-              海外はドバイ、バンコク、シドニー、ロンドン、パリと住んできまして、フランス語やタイ語を含む４か国語を話します。今はアラビア語の習得に向けて励んでいます。`];
-const aboutPics = ["../resource/Akifumi-thailand.png","../resource/CA2.png"];
-const aboutYoutube = [`<iframe src="https://www.youtube.com/embed/i44_vjJfeVI?si=iW5KSZXlnPz3MQpb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-`,`<iframe src="https://www.youtube.com/embed/bk6rzt08i0Q?si=d6Jevm7H1MzIPE4G" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`];
+import { textTitle, textContent, aboutPics, aboutYoutube } from "../common/Variable.js";
 
 export class About extends Component{
   render(){
--- src/js/components/Voice.js
+++ src/js/components/Voice.js
@@ -1,39 +1,6 @@
 import { Component } from "../common/component.js";
 import { Pictures, voiceTitle, customerVoice } from "../common/Variable.js"
 
-/* const Pictures = [
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/yLFTjinxeHg?si=WEMLY88Fzu5uZcLS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/ivwPWI8w6RA?si=2lXD5crW9yAi6pD0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/5_HuhAKKtCg?si=EyMMkVjFpertQmZm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/uq4SHpbhPLU?si=6fzfkoHgcC0klacG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/ghzjtepxbhE?si=zsR7D-4O74vG1_R2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/XVv0xnIhaRA?si=DpbNbBhIjEArRZl0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  `<iframe width="560" height="315" src="https://www.youtube.com/embed/dfyB0Pyxo14?si=k7MwICbhFP1UfR-x" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-  "../resource/VoicePic8.png"
-];
- 
-const voiceTitle = [
-  "4人のママで電気工事現場監督（神奈川）",
-  "アパレルデザイナー & スタイリスト（京都＆広島）",
-  "ピラティススタジオ経営（福岡）",
-  "ラーメン店４店舗経営ご夫婦（東京）",
-  "EC販売代行（神奈川）",
-  "パティスリー経営（熊本）",
-  "リンパマッサージサロン経営（東京）",
-  "お花屋さん（熊本）"
-]
-const customerVoice = [
-  "子育て真っ最中だが、次の人生に踏み出そうと英語を開始！一緒に渡豪もし、どんどん才能開花中！",
-  "とにかく何か踏み出したい！ということで英語を始めたら、転職が起こるわ息子も英語が話せるようになるわミラクルいっぱいです！",
-  "英語が全く聞こえなかったのが１ヶ月で聞こえるようになった。毎日が海外で生活しているようだった。",
-  "",
-  "",
-  "",
-  "",
-  "高校時代から嫌いなままだったが、英語のでのチャットやヴォイスメッセージを自然に送れるようになった。"
-]
-*/
-
 export class Voice extends Component{
   render(){
 
